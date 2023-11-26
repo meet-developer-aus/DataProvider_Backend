8e929bf7c46cf6aa655b896ee355a3c7b3bd4126
romeoantony53 <romeorealist@gmail.com>
2023-11-26 09:00:32 +0000

    Loaded API key from .env variable and set the API client ready with API endpoint URI,

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 
 require_once 'vendor/autoload.php';
 
-//use ATDWClientNameSpace\ApiClient;
+//load API key from .env for security and avoid exposure
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-// Create Apiclient instance 
-$apiClientObj = new ATDWClientNameSpace\ApiClient();
+//API key
+$apiKey=$_ENV['API_KEY'];
+
+
+//use ATDWClientNameSpace\ApiClient;/
+
+// Create Apiclient instance by passing API key
+$apiClientObj = new ATDWClientNameSpace\ApiClient($apiKey);
 
 
 // Fetch API data by ApiClient by bew Apiclient instance 
--- src/ATDWClientNameSpace/ApiClient.php
+++ src/ATDWClientNameSpace/ApiClient.php
@@ -1,5 +1,6 @@
 <?php
 namespace ATDWClientNameSpace;
+use GuzzleHttp\Client;
 
 // creating API request client class named ApiClient with properties + methods
 
@@ -7,15 +8,24 @@ namespace ATDWClientNameSpace;
 class ApiClient
 {
 
-// properties 
-
+// properties  apikay,API url, and httpclient
+private $apiKey;
+private $baseUri = 'https://atlas.atdw-online.com.au/api/atlas/';
+private $httpClient;
 
 //methods 
 
 // class constructor 
-public function __construct()
+public function __construct($apiKey)
     {
         echo "ApiClient constructor is called";
+
+        // Assign API key to ApiCLient's objetct apiKey property during object creation 
+        $this->apiKey = $apiKey;
+
+
+        //create httpClient which get API endpoint 
+        $this->httpClient = new Client(['base_uri' => $this->baseUri]);
        
     }
 
