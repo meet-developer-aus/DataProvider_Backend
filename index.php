a9ab6d336424332113ab4d6b92a17e0c4e616700
romeoantony53 <romeorealist@gmail.com>
2023-11-30 07:31:43 +0000

    changes in docker file and  docker-compose and app runs perfectly on docker muti container setup

--- index.php
+++ index.php
@@ -1,16 +1,47 @@
 <?php
-// Set CORS headers
-header("Access-Control-Allow-Origin: *"); // need to replace with actual frontend URL 
+header("Access-Control-Allow-Origin: *"); // Replace with actual frontend URL
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-
+header("Access-Control-Allow-Credentials: true");
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 
 require_once 'vendor/autoload.php';
 
+
+
+
+// return  data as json data..
+
+// Check if the request URI contains "/api"
+if (strpos($_SERVER['REQUEST_URI'], '/api') !== false) {
+    // Handle API request
+    handleApiRequest();
+} else {
+    // Handle regular request
+    handleRegularRequest();
+}
+
+// existing code for handling regular requests
+function handleRegularRequest() {
+    // ...  existing code for regular requests ...
+}
+
+//  API endpoint logic
+function handleApiRequest() {
+    // ...  API endpoint logic ...
+    // For example, you can echo a JSON response
+
 //load API key from .env for security and avoid exposure
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+
+
 
 //API key
 $apiKey = $_ENV['API_KEY'];
@@ -62,8 +93,6 @@ $jsonResponse = json_encode($responseData, JSON_PRETTY_PRINT);
 // Output the JSON response
 header('Content-Type: application/json');
 echo $jsonResponse;
-
-
-// return  data as json data..
-
+    
+}
 ?>
